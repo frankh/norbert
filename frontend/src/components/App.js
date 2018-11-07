77591f88452988bf518e154f1e3294861b23ccc5
Frank Hamand <frankhamand@gmail.com>
2018-11-07 22:52:43 +0000

    Start of redesign

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from "react-apollo";
 import { Layout } from "antd";
+import { Menu, Icon, Button } from 'antd';
 import ServiceList from './ServiceList.js';
 import { Route, Switch, withRouter } from "react-router-dom";
 import CheckPage from './CheckPage.js';
@@ -10,23 +11,44 @@ import { GET_SERVICES } from "../queries";
 import "antd/dist/antd.css";
 import './App.css';
 
-const { Header, Footer, Content } = Layout;
+const { Header, Sider, Content } = Layout;
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-            <Layout style={{backgroundColor: "inherit"}}>
-                <Header />
-                <Content>
-                    <Switch>
-                        <Route path="/check/:id/" component={CheckPage} />
-                        <Route path="/" component={ServiceList}/>
-                    </Switch>
-                </Content>
-                <Footer />
-            </Layout>
+        <Layout>
+          <Sider>
+            <div class="logo-title">
+              <img src="logo.png"></img>
+              <h1>Norbert</h1>
+            </div>
+            <Menu
+              defaultSelectedKeys={['services']}
+              mode="inline"
+              theme="light"
+            >
+              <Menu.Item key="services">
+                <Icon type="deployment-unit" theme="outlined" />
+                <span>Services</span>
+              </Menu.Item>
+              <Menu.Item key="checks">
+                <Icon type="fork" theme="outlined" />
+                <span>Checks</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout style={{backgroundColor: "inherit"}}>
+              <Header />
+              <Content>
+                  <Switch>
+                      <Route path="/check/:id/" component={CheckPage} />
+                      <Route path="/" component={ServiceList}/>
+                  </Switch>
+              </Content>
+          </Layout>
+        </Layout>
       </div>
     );
   }
